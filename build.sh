07c4382ce4a657dc5ce4557f0975cbbd3ce9671c
Ethan Buchman <ethan@coinculture.info>
2015-07-09 19:11:18 +0000

    namespacing for container tests

--- build.sh
+++ build.sh
@@ -1,20 +1,22 @@
 #! /bin/bash
 
+# $PREFIX to allow namespacing instances of the app. Default is empty
+
 # run tendermint (with data container)
-docker run --name mintdata eris/mint mkdir -p /home/eris/.eris/blockchains/tendermint
+docker run --name "${PREFIX}mintdata" eris/mint mkdir -p /home/eris/.eris/blockchains/tendermint
 if [ "$VC" != "" ]; then
 	cd $VC
-	tar cf - . | docker run -i --rm --volumes-from mintdata --user eris eris/mint tar xvf - -C /home/eris/.eris/blockchains/tendermint
+	tar cf - . | docker run -i --rm --volumes-from "${PREFIX}mintdata" --user eris eris/mint tar xvf - -C /home/eris/.eris/blockchains/tendermint
 fi
-docker run --name mint --volumes-from mintdata -d -p 46657:46657 -e FAST_SYNC=$FAST_SYNC eris/mint
+docker run --name "${PREFIX}mint" --volumes-from "${PREFIX}mintdata" -d -p 46657:46657 -e FAST_SYNC=$FAST_SYNC eris/mint
 
 # build and run tinydns
 cd $GOPATH/src/github.com/eris-ltd/mindy/tinydns
-docker build -t tinydns .
-docker run --name tinydns -d -p 53:53/udp tinydns
+docker build -t "${PREFIX}tinydns" .
+docker run --name "${PREFIX}tinydns" -d -p 53:53/udp "${PREFIX}tinydns"
 
 # build and run mindy
 cd $GOPATH/src/github.com/eris-ltd/mindy
-docker build -t mindy .
-docker run -d --name mindy --volumes-from tinydns --link mint:mint -e UPDATE_EVERY=$UPDATE_EVERY mindy 
+docker build -t "${PREFIX}mindy" .
+docker run -d --name "${PREFIX}mindy" --volumes-from "${PREFIX}tinydns" --link "${PREFIX}mint":mint -e UPDATE_EVERY=$UPDATE_EVERY "${PREFIX}mindy"
 
--- test/build.sh
+++ test/build.sh
@@ -1,12 +1,13 @@
 #! /bin/sh
 
+export PREFIX=mindy_test_
 cd $GOPATH/src/github.com/eris-ltd/mindy
 VC=./test UPDATE_EVERY=5 ./build.sh
 
 cd $GOPATH/src/github.com/eris-ltd/mindy
 docker build -t mindy_test -f test/Dockerfile .
-docker run --rm -t --link mint:mint --link tinydns:tiny --name mindy_test mindy_test
+docker run --rm -t --link "${PREFIX}mint":mint --link "${PREFIX}tinydns":tiny --name mindy_test mindy_test
 
 echo "----------------------------------"
 echo "cleanup ..."
-docker rm -f mindy tinydns mint mintdata
+docker rm -f "${PREFIX}mindy" "${PREFIX}tinydns" "${PREFIX}mint" "${PREFIX}mintdata"
