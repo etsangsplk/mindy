9434e9a3f6ba92372a79ac0326a292a8b0eded2a
Ethan Buchman <ethan@coinculture.info>
2015-10-08 03:48:07 +0000

    circle badge

--- test/plumbing/run.sh
+++ test/plumbing/run.sh
@@ -1,5 +1,13 @@
 #! /bin/bash
 
+ifExit(){
+	if [ $? -ne 0 ]; then
+		echo "ifExit: $1"
+		exit 1
+	fi
+}
+
+
 if [ "$PREFIX" = "" ]; then
 	PREFIX=mindy_test_
 fi
@@ -13,26 +21,31 @@ UPDATE_EVERY=5
 # run tendermint (with data container)
 docker run --name "${PREFIX}mintdata" eris/mint mkdir -p /home/eris/.eris/blockchains/tendermint
 if [ "$VC" != "" ]; then
-	echo "copy data into eris/mint contract"
+	echo "copy data into eris/mint container"
 	cd $VC
 	tar cf - . | docker run -i --rm --volumes-from "${PREFIX}mintdata" --user eris eris/mint tar xvf - -C /home/eris/.eris/blockchains/tendermint
 fi
 echo "###################### RUN eris/mint container #########################"
 docker run --name "${PREFIX}mint" --volumes-from "${PREFIX}mintdata" -d -p 46657:46657 -e FAST_SYNC=$FAST_SYNC eris/mint
+ifExit "could not start eris/mint container"
 
 # run tinydns
 echo "###################### RUN tinydns container #########################"
-docker run --name "${PREFIX}tinydns" -d -p 53:53/udp "${PREFIX}tinydns"
+docker run --name "${PREFIX}tinydns" -d "${PREFIX}tinydns"
+ifExit "could not start tinydns container"
 
 # run mindy
 echo "###################### RUN mindy container #########################"
 docker run -d --name "${PREFIX}mindy" --volumes-from "${PREFIX}tinydns" --link "${PREFIX}mint":mint -e UPDATE_EVERY=$UPDATE_EVERY "${PREFIX}mindy"
+ifExit "could not start mindy container"
 
 
 echo "###################### RUN the mindy test #########################"
-docker run --rm -t --link "${PREFIX}mint":mint --link "${PREFIX}tinydns":tiny --name mindy_test mindy_test
+docker run -t --link "${PREFIX}mint":mint --link "${PREFIX}tinydns":tiny --name mindy_test mindy_test
+ifExit "could not start test container"
 
 echo "----------------------------------"
 echo "cleanup ..."
-docker rm -f "${PREFIX}mindy" "${PREFIX}tinydns" "${PREFIX}mint" "${PREFIX}mintdata"
+docker rm -f "${PREFIX}mindy" "${PREFIX}tinydns" "${PREFIX}mint" "${PREFIX}mintdata" mindy_test
+
 
