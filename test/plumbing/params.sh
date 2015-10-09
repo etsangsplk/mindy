fed834e4d50a3b978e17ae9f1583f373abc2ab68
Ethan Buchman <ethan@coinculture.info>
2015-10-09 19:01:55 +0000

    test with docker-machine

---
+++ test/plumbing/params.sh
@@ -0,0 +1,9 @@
+
+# path in the GOPATH
+export base=github.com/eris-ltd/mindy
+
+# scripts for building containers and running dependency containers
+export build_script=test/plumbing/build.sh
+
+# we use this for building and running the base
+export docker_latest="1.8.2"
