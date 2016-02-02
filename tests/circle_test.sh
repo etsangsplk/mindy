22f98b99192ecc50baf19f382d20f0204d70a763
Casey Kuhlman <casey@erisindustries.com>
2016-02-02 03:19:01 +0100

    clean up circle test runner

--- tests/circle_test.sh
+++ tests/circle_test.sh
@@ -22,7 +22,6 @@
 
 uuid=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 12 | head -n 1)
 machine="eris-test-mindy-$uuid"
-ver=$(cat version/version.go | tail -n 1 | cut -d ' ' -f 4 | tr -d '"')
 start=`pwd`
 
 # ----------------------------------------------------------
@@ -34,12 +33,6 @@ eval $(docker-machine env $machine)
 echo "Machine setup."
 echo
 docker version
-echo
-echo "Pulling needed images"
-docker pull quay.io/eris/data 1>/dev/null
-docker pull quay.io/eris/keys 1>/dev/null
-docker pull quay.io/eris/erisdb:$ver 1>/dev/null
-echo "Pulling finished."
 echo
 
 # ----------------------------------------------------------
